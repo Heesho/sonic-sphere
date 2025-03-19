21f537f38300096652db02a32f26f5a4a21e4f12
Heesho <hisham11027@gmail.com>
2025-03-18 18:46:39 -0700

    updates to plugin contract, next is work on multicall

--- contracts/PluginFactory.sol
+++ contracts/PluginFactory.sol
@@ -2,78 +2,82 @@
 pragma solidity 0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "contracts/interfaces/IGauge.sol";
-import "contracts/interfaces/IBribe.sol";
 import "contracts/interfaces/IVoter.sol";
 
-// TODO:
-// - make it ownable
-// - make a treasury
-// - add balance of and totalSupply back
-// - add funciton to setTreasury
-// - think about other settable variables (duration, initial price, etc.) 
-// - why is minInitPrice immutable?
-// - review fee flow again to see what they got
-// - remove the bribe token, probably only add them if requested
-// - on buy do i want to let them input basket of assets? In case someone sends assets to contract?
-// - a bunch of testing
-contract Plugin is ReentrancyGuard, Ownable {
+contract Plugin {
     using SafeERC20 for IERC20;
 
     /*----------  CONSTANTS  --------------------------------------------*/
 
-    uint256 constant public AMOUNT = 1e18;
-    uint256 constant public PRECISION = 1e18;
-    uint256 constant public DURATION = 7 days;
-    uint256 constant public ABS_MAX_INIT_PRICE = type(uint192).max;
-    uint256 constant public PRICE_MULITPLIER = 2e18;
+    uint256 constant public GAUGE_DEPOSIT_AMOUNT = 1e18;
+    uint256 public constant MIN_EPOCH_PERIOD = 1 hours;
+    uint256 public constant MAX_EPOCH_PERIOD = 365 days;
+    uint256 public constant MIN_PRICE_MULTIPLIER = 1.1e18; // Should at least be 110% of settlement price
+    uint256 public constant MAX_PRICE_MULTIPLIER = 3e18; // Should not exceed 300% of settlement price
+    uint256 public constant ABS_MIN_INIT_PRICE = 1e6; // Minimum sane value for init price
+    uint256 public constant ABS_MAX_INIT_PRICE = type(uint192).max; // chosen so that initPrice * priceMultiplier does not exceed uint256
+    uint256 public constant PRICE_MULTIPLIER_SCALE = 1e18;
 
     /*----------  STATE VARIABLES  --------------------------------------*/
 
-    IERC20 private immutable token; // payment token
     address private immutable OTOKEN;
     address private immutable voter;
     address private gauge;
     address private bribe;
-
-    string private  protocol;
     string private name;
-    
     address[] private assetTokens;
-    address[] private bribeTokens;
-
-    address public treasury;
-    uint256 public immutable minInitPrice;
     bool public initialized = false;
 
-    struct Auction {
-        uint256 epochId;
-        uint256 initPrice;
-        uint256 startTime;
+    IERC20 public immutable paymentToken;
+    address public immutable paymentReceiver;
+    uint256 public immutable epochPeriod;
+    uint256 public immutable priceMultiplier;
+    uint256 public immutable minInitPrice;
+
+    struct Slot0 {
+        uint8 locked; // 1 if unlocked, 2 if locked
+        uint16 epochId; // intentionally overflowable
+        uint192 initPrice;
+        uint40 startTime;
     }
 
-    Auction internal auction;
+    Slot0 internal slot0;
 
     /*----------  ERRORS ------------------------------------------------*/
 
-    error Plugin__InvalidZeroInput();
+    error Plugin__Initialized();
     error Plugin__NotAuthorizedVoter();
     error Plugin__DeadlinePassed();
     error Plugin__EpochIdMismatch();
     error Plugin__MaxPaymentAmountExceeded();
-    error Plugin__Initialized();
+    error Plugin__EmptyAssets();
+    error Plugin__Reentrancy();
+    error Plugin__InitPriceBelowMin();
+    error Plugin__InitPriceExceedsMax();
+    error Plugin__EpochPeriodBelowMin();
+    error Plugin__EpochPeriodExceedsMax();
+    error Plugin__PriceMultiplierBelowMin();
+    error Plugin__PriceMultiplierExceedsMax();
+    error Plugin__MinInitPriceBelowMin();
+    error Plugin__MinInitPriceExceedsAbsMaxInitPrice();
+    error Plugin__PaymentReceiverIsThis();
 
     /*----------  EVENTS ------------------------------------------------*/
 
-    event Plugin__ClaimedAndDistributed(address indexed treasury, uint256 amount);
-    event Plugin__Buy(address indexed buyer, address indexed assetReceiver, uint256 paymentAmount);
+    event Plugin__Buy(address indexed buyer, address indexed assetsReceiver, uint256 paymentAmount);
 
     /*----------  MODIFIERS  --------------------------------------------*/
 
-    modifier nonZeroInput(uint256 _amount) {
-        if (_amount == 0) revert Plugin__InvalidZeroInput();
+    modifier nonReentrant() {
+        if (slot0.locked == 2) revert Plugin__Reentrancy();
+        slot0.locked = 2;
+        _;
+        slot0.locked = 1;
+    }
+
+    modifier nonReentrantView() {
+        if (slot0.locked == 2) revert Plugin__Reentrancy();
         _;
     }
 
@@ -85,62 +89,70 @@ contract Plugin is ReentrancyGuard, Ownable {
     /*----------  FUNCTIONS  --------------------------------------------*/
 
     constructor(
-        address _token, 
         address _voter, 
         address[] memory _assetTokens, 
-        address[] memory _bribeTokens,
-        string memory _protocol,
         string memory _name,
-        uint256 _initPrice,
-        uint256 _minInitPrice,
-        address _treasury
+        uint256 initPrice,
+        address paymentToken_,
+        address paymentReceiver_,
+        uint256 epochPeriod_,
+        uint256 priceMultiplier_,
+        uint256 minInitPrice_
     ) {
-        token = IERC20(_token);
+        if (initPrice < minInitPrice_) revert Plugin__InitPriceBelowMin();
+        if (initPrice > ABS_MAX_INIT_PRICE) revert Plugin__InitPriceExceedsMax();
+        if (epochPeriod_ < MIN_EPOCH_PERIOD) revert Plugin__EpochPeriodBelowMin();
+        if (epochPeriod_ > MAX_EPOCH_PERIOD) revert Plugin__EpochPeriodExceedsMax();
+        if (priceMultiplier_ < MIN_PRICE_MULTIPLIER) revert Plugin__PriceMultiplierBelowMin();
+        if (priceMultiplier_ > MAX_PRICE_MULTIPLIER) revert Plugin__PriceMultiplierExceedsMax();
+        if (minInitPrice_ < ABS_MIN_INIT_PRICE) revert Plugin__MinInitPriceBelowMin();
+        if (minInitPrice_ > ABS_MAX_INIT_PRICE) revert Plugin__MinInitPriceExceedsAbsMaxInitPrice();
+        if (paymentReceiver_ == address(this)) revert Plugin__PaymentReceiverIsThis();
+
         voter = _voter;
         assetTokens = _assetTokens;
-        bribeTokens = _bribeTokens;
-        protocol = _protocol;
         name = _name;
         OTOKEN = IVoter(_voter).OTOKEN();
 
-        auction.initPrice = _initPrice;
-        auction.startTime = block.timestamp;
+        slot0.initPrice = uint192(initPrice);
+        slot0.startTime = uint40(block.timestamp);
 
-        minInitPrice = _minInitPrice;
-        treasury = _treasury;
+        paymentToken = IERC20(paymentToken_);
+        paymentReceiver = paymentReceiver_;
+        epochPeriod = epochPeriod_;
+        priceMultiplier = priceMultiplier_;
+        minInitPrice = minInitPrice_;
     }
 
-    function claimAndDistribute() public virtual nonReentrant {
-        uint256 balance = token.balanceOf(address(this));
-        if (balance > 0) {
-            token.safeTransfer(treasury, balance);
-            emit Plugin__ClaimedAndDistributed(treasury, balance);
-        }
-    }
-
-    function buy(address assetReceiver, uint256 epochId, uint256 deadline, uint256 maxPaymentTokenAmount) external nonReentrant returns (uint256 paymentAmount) {
+    function buy(
+        address[] calldata assets,
+        address assetsReceiver,
+        uint256 epochId,
+        uint256 deadline,
+        uint256 maxPaymentTokenAmount
+    ) external nonReentrant returns (uint256 paymentAmount) {
         if (block.timestamp > deadline) revert Plugin__DeadlinePassed();
+        if (assets.length == 0) revert Plugin__EmptyAssets();
 
-        Auction memory auctionCache = auction;
+        Slot0 memory slot0Cache = slot0;
 
-        if (epochId != auctionCache.epochId) revert Plugin__EpochIdMismatch();
+        if (uint16(epochId) != slot0Cache.epochId) revert Plugin__EpochIdMismatch();
 
-        paymentAmount = getPriceFromCache(auctionCache);
+        paymentAmount = getPriceFromCache(slot0Cache);
 
         if (paymentAmount > maxPaymentTokenAmount) revert Plugin__MaxPaymentAmountExceeded();
 
         if (paymentAmount > 0) {
-            token.safeTransferFrom(msg.sender, address(this), paymentAmount);
+            paymentToken.safeTransferFrom(msg.sender, paymentReceiver, paymentAmount);
         }
 
-        // change this to just oTOKEN
-        IGauge(gauge).getReward(address(this));
-        uint256 balance = IERC20(OTOKEN).balanceOf(address(this));
-        if (balance > 0) {
-            IERC20(OTOKEN).safeTransfer(assetReceiver, balance);
+        for (uint256 i = 0; i < assets.length; i++) {
+            uint256 balance = IERC20(assets[i]).balanceOf(address(this));
+            IERC20(assets[i]).safeTransfer(assetsReceiver, balance);
         }
 
-        uint256 newInitPrice = paymentAmount * PRICE_MULITPLIER / PRECISION;
+        // Setup new auction
+        uint256 newInitPrice = paymentAmount * priceMultiplier / PRICE_MULTIPLIER_SCALE;
 
         if (newInitPrice > ABS_MAX_INIT_PRICE) {
             newInitPrice = ABS_MAX_INIT_PRICE;
@@ -148,13 +160,17 @@ contract Plugin is ReentrancyGuard, Ownable {
             newInitPrice = minInitPrice;
         }
 
-        auctionCache.epochId++;
-        auctionCache.initPrice = newInitPrice;
-        auctionCache.startTime = block.timestamp;
+        // epochID is allowed to overflow, effectively reusing them
+        unchecked {
+            slot0Cache.epochId++;
+        }
+        slot0Cache.initPrice = uint192(newInitPrice);
+        slot0Cache.startTime = uint40(block.timestamp);
 
-        auction = auctionCache;
+        // Write cache in single write
+        slot0 = slot0Cache;
 
-        emit Plugin__Buy(msg.sender, assetReceiver, paymentAmount);
+        emit Plugin__Buy(msg.sender, assetsReceiver, paymentAmount);
 
         return paymentAmount;
     }
@@ -162,7 +178,7 @@ contract Plugin is ReentrancyGuard, Ownable {
     function initialize() external {
         if (initialized) revert Plugin__Initialized();
         initialized = true;
-        IGauge(gauge)._deposit(address(this), AMOUNT);
+        IGauge(gauge)._deposit(address(this), GAUGE_DEPOSIT_AMOUNT);
     }
 
     /*----------  RESTRICTED FUNCTIONS  ---------------------------------*/
@@ -175,32 +191,35 @@ contract Plugin is ReentrancyGuard, Ownable {
         bribe = _bribe;
     }
 
-    function getPriceFromCache(Auction memory auctionCache) internal view returns (uint256) {
-        uint256 timeElapsed = block.timestamp - auctionCache.startTime;
+    /// @dev Retrieves the current price from the cache based on the elapsed time since the start of the epoch.
+    /// @param slot0Cache The Slot0 struct containing the initial price and start time of the epoch.
+    /// @return price The current price calculated based on the elapsed time and the initial price.
+    /// @notice This function calculates the current price by subtracting a fraction of the initial price based on the elapsed time.
+    // If the elapsed time exceeds the epoch period, the price will be 0.
+    function getPriceFromCache(Slot0 memory slot0Cache) internal view returns (uint256) {
+        uint256 timePassed = block.timestamp - slot0Cache.startTime;
 
-        if (timeElapsed > DURATION) {
+        if (timePassed > epochPeriod) {
             return 0;
         }
 
-        return auctionCache.initPrice - (auctionCache.initPrice * timeElapsed / DURATION);
+        return slot0Cache.initPrice - slot0Cache.initPrice * timePassed / epochPeriod;
     }
+
 
     /*----------  VIEW FUNCTIONS  ---------------------------------------*/
 
-    function getPrice() external view returns (uint256) {
-        return getPriceFromCache(auction);
+    /// @dev Calculates the current price
+    /// @return price The current price calculated based on the elapsed time and the initial price.
+    /// @notice Uses the internal function `getPriceFromCache` to calculate the current price.
+    function getPrice() external view nonReentrantView returns (uint256) {
+        return getPriceFromCache(slot0);
     }
 
-    function getAuction() external view returns (Auction memory) {
-        return auction;
-    }
-
-    function getToken() public view virtual returns (address) {
-        return address(token);
-    }
-
-    function getProtocol() public view virtual returns (string memory) {
-        return protocol;
+    /// @dev Retrieves Slot0 as a memory struct
+    /// @return Slot0 The Slot0 value as a Slot0 struct
+    function getSlot0() external view nonReentrantView returns (Slot0 memory) {
+        return slot0;
     }
 
     function getName() public view virtual returns (string memory) {
@@ -223,15 +242,9 @@ contract Plugin is ReentrancyGuard, Ownable {
         return assetTokens;
     }
 
-    function getBribeTokens() public view returns (address[] memory) {
-        return bribeTokens;
-    }
-
 }
 
 contract PluginFactory {
-
-    string public constant PROTOCOL = 'Voter Owned Liquidity';
 
     address public immutable VOTER;
 
@@ -244,30 +257,28 @@ contract PluginFactory {
     }
 
     function createPlugin(
-        address _paymentToken,
         address[] memory _assetTokens,
         string memory _name,
-        uint256 _initPrice,
-        uint256 _minInitPrice,
-        address _treasury
+        uint256 initPrice,
+        address paymentToken_,
+        address paymentReceiver_,
+        uint256 epochPeriod_,
+        uint256 priceMultiplier_,
+        uint256 minInitPrice_
     ) external returns (address) {
 
-        address[] memory bribeTokens = new address[](1);
-        bribeTokens[0] = _paymentToken;
-
         Plugin lastPlugin = new Plugin(
-            _paymentToken,
             VOTER,
             _assetTokens,
-            bribeTokens,
-            PROTOCOL,
             _name,
-            _initPrice,
-            _minInitPrice,
-            _treasury
+            initPrice,
+            paymentToken_,
+            paymentReceiver_,
+            epochPeriod_,
+            priceMultiplier_,
+            minInitPrice_
         );
         last_plugin = address(lastPlugin);
-        lastPlugin.transferOwnership(msg.sender);
         emit PluginFactory__PluginCreated(last_plugin);
         return last_plugin;
     }
--- contracts/interfaces/IPlugin.sol
+++ contracts/interfaces/IPlugin.sol
@@ -2,22 +2,32 @@
 pragma solidity 0.8.19;
 
 interface IPlugin {
+    struct Slot0 {
+        uint8 locked;
+        uint16 epochId;
+        uint192 initPrice;
+        uint40 startTime;
+    }
     /*----------  FUNCTIONS  --------------------------------------------*/
-    function claimAndDistribute() external;
+    function buy(address[] calldata assets, address assetsReceiver, uint256 epochId, uint256 deadline, uint256 maxPaymentTokenAmount) external returns (uint256);
     /*----------  RESTRICTED FUNCTIONS  ---------------------------------*/
     function setGauge(address gauge) external;
     function setBribe(address bribe) external;
     /*----------  VIEW FUNCTIONS  ---------------------------------------*/
-    function balanceOf(address account) external view returns (uint256);
-    function totalSupply() external view returns (uint256);
-    function getToken() external view returns (address);
-    function getProtocol() external view returns (string memory);
     function getName() external view returns (string memory);
     function getVoter() external view returns (address);
     function getGauge() external view returns (address);
     function getBribe() external view returns (address);
     function getAssetTokens() external view returns (address[] memory);
     function getBribeTokens() external view returns (address[] memory);
+    function getPrice() external view returns (uint256);
+    function getSlot0() external view returns (Slot0 memory);
+    function initialized() external view returns (bool);
+    function paymentToken() external view returns (address);
+    function paymentReceiver() external view returns (address);
+    function epochPeriod() external view returns (uint256);
+    function priceMultiplier() external view returns (uint256);
+    function minInitPrice() external view returns (uint256);
 }
 
 
